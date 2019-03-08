885deaa6a53b0043bb9f45f947e311ff358b7201
KieranVieira <kieranvieira@live.com>
2019-03-07 20:56:19 -0500

    Added test for delete user

--- api/routes/userRouter.spec.js
+++ api/routes/userRouter.spec.js
@@ -4,20 +4,39 @@ const server = require('../server.js');
 const db = require('../../database/dbConfig.js')
 
 describe('User Router', () => {
-    afterEach(async () => {
-        await db('hobbits').truncate()
-    })
     describe('Post /api/users', () => {
+        afterEach(async () => {
+            await db('users').truncate()
+        })
+
         it('Should insert user to database', async() => {
-            const res = await request(server).post('/').send({
+            const res = await request(server).post('/api/users').send({
                 username: 'kieran', 
                 password: 'pass' 
             });
 
-            expect(res.body).toBe({ id: 1 })
+            expect(res.body).toEqual([1])
+        });
+
+        it('Should return JSON', async() => {
+            const res = await request(server).post('/api/users').send({
+                username: 'test', 
+                password: 'pass' 
+            });
+
+            expect(res.type).toBe('application/json')
         });
     });
     describe('Delete /api/users/:id', () => {
-        
+        it('Should add user then remove user', async() => {
+            await request(server).post('/api/users').send({
+                username: 'kieran', 
+                password: 'pass' 
+            });
+
+            const res = await request(server).del('/api/users/1')
+
+            expect(res.status).toBe(204);
+        });
     });
 });
